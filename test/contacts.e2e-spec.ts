b5552d101e561094a194d312b3b9c10ff446e5b2
anil.pandey@WWW.INNODEED.COM <anil.pandey@WWW.INNODEED.COM>
2021-08-08 22:56:01 -0700

    added e2e test cases

--- test/contacts.e2e-spec.ts
+++ test/contacts.e2e-spec.ts
@@ -2,10 +2,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { ContactsModule } from 'src/contacts/contacts.module';
+import { ContactsModule } from '../src/contacts/contacts.module';
 import { getRepositoryToken } from '@nestjs/typeorm';
-import { contactStub } from 'src/contacts/contacts/stubss/contact.stub';
-jest.mock("./contacts.service");
+import { contactStub } from '../src/contacts/contacts/stubss/contact.stub';
+import { Contact } from '../src/contacts/contacts/contact.entity';
+import { ContactsService } from '../src/contacts/contacts/__mocks__/contacts.service';
+import { DeleteResult, UpdateResult } from 'typeorm';
+//jest.mock("../src/contacts.service");
 
 describe('ContactsController (e2e)', () => {
   let app: INestApplication;
@@ -13,7 +16,7 @@ describe('ContactsController (e2e)', () => {
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [ContactsModule],
-    }).compile();
+    }).overrideProvider(getRepositoryToken(Contact)).useValue(ContactsService).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
@@ -22,8 +25,47 @@ describe('ContactsController (e2e)', () => {
   it('Contacts/ (GET)', () => {
     return request(app.getHttpServer())
       .get('/contacts')
-      .expect(200)
       .expect(contactStub)
       //.expect('Hello World!');
   });
+  it('Contacts/create (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/contacts/create')
+      .send({
+        id:1,
+        firstName: "sakshi",
+        lastName:"dgjh",
+        phone:"dhgj",
+        email:"dgj",
+        city:"ufyh",
+        country:"fhvj"
+      })
+      .expect(contactStub)
+      //.expect('Hello World')
+  })
+  it('Contacts/:id/update (PUT)', () => {
+    return request(app.getHttpServer())
+      .put('/contacts/:id/update')
+          .send({
+            id:1,
+            firstName: "sakshi",
+            lastName:"dgjh",
+            phone:"dhgj",
+            email:"dgj",
+            city:"ufyh",
+            country:"fhvj"
+          })
+      .expect(contactStub)
+      //.expect('Hello World')
+  })
+  it('Contacts/:id/delete (DELETE)', () => {
+    return request(app.getHttpServer())
+      .delete('/contacts/:id/delete')
+      .send({
+        id:1
+      })
+      
+      .expect(DeleteResult)
+      //.expect('Hello World')
+  })
 });
